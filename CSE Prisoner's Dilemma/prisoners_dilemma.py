86e63d941e9b1e714fd2493938ff6a886f7892ef
marcnpalmer <marcnpalmer@gmail.com>
2014-07-09 10:03:48 -0400

    Wet Bandits
    winning!

--- CSE Prisoner's Dilemma/prisoners_dilemma.py
+++ CSE Prisoner's Dilemma/prisoners_dilemma.py
@@ -219,17 +219,22 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Wet Bandits'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
+            size = len(history)
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
-    
+                if history[-1]=='c' and opponent_history[-1]=='b':
+                    return 'b' # betray is they were severely punished last time
+                else:
+                    if(size%3==0): #the number of rounds played is a multiple of 3
+                        return 'c'
+                    else:
+                        return 'b'
+        
     
     
     
